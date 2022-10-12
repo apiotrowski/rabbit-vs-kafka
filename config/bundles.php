cedb591cda62ffb7df02d26f7c8039e441ba1fb9
Andrzej Piotrowski <kontakt@stronawww.net>
2022-10-12 12:52:04 +0200

    Dodanie zmian zwiazanych z kafka

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
 ];
